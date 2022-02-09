fc0e9d2a262d5f37318706ac3c7b4011350daa7a
noellecrow <crownoelle@gmail.com>
2022-02-08 19:06:45 -0800

    linked tasty api with new merges

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
+// Global Variables
 var buttonEl = document.querySelector("#joke");
 var jokeArea = document.querySelector(".joke-area");
 var ingreClear = document.querySelector("#ingre-clear");
 var saveIngre = document.querySelector("#save-ingredient");
 var ingredient = document.querySelector("#ingredient-list");
-var input = document.querySelector(".input")
-// buttonEl.addEventListener("click", function(){
+var input = document.querySelector(".input");
+
+// Humor API - joke shows on button click
+buttonEl.addEventListener("click", function(){
 
   fetch('https://api.humorapi.com/jokes/random/?api-key=18cc987c5ed94f4d9d01dfbbec17b43a&include-tags=food&max-length=140')
   .then(function(response) {
@@ -21,42 +24,21 @@ var input = document.querySelector(".input")
   console.log(error);
 });
 
-
+})
 
 //ingreClear.addEventListener("click", localStorage.clear);
 
-// list of ingredients created
-//push and convert data from array to string prototype join
+
 var createTaskHandler = function() {
 var listItemEl = document.createElement ("li");
-  listItemEl.className = "ingredient-item";
+  listItemEl.className = "input";
   listItemEl.textContent = "";
   ingredient.appendChild(listItemEl);
-  console.log(input.value)
+  console.log(value)
 
 
-fetch(`https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=${input.value}`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "tasty.p.rapidapi.com",
-		"x-rapidapi-key": "0cd388e86emsh38cba0e720e1220p1e0b48jsn82e9770dc6c0"
-	}
-})
-.then(response => {
-	console.log(response);
-  return response.json()
-}).then(data => {
-  console.log(data);
-})
 
-.then(function(JSONResponse) {
-  console.log(JSONResponse);
-  var printThis = JSON.parse(JSONResponse);
-  console.log(printThis);
-})
-.catch(function(error) {
-  console.log(error);
-});
+}
 
 
 console.log("js is running");
@@ -96,17 +78,32 @@ function addToList(item, counter){
     li.appendChild(document.createTextNode(item));
     li.appendChild(delBtn);
     ul.appendChild(li);
+
+    fetch(`https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=${item.value}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "tasty.p.rapidapi.com",
+		"x-rapidapi-key": "0cd388e86emsh38cba0e720e1220p1e0b48jsn82e9770dc6c0"
+	}
+})
+.then(response => {
+	console.log(response);
+  return response.json()
+}).then(data => {
+  console.log(data);
+})
+
+.then(function(JSONResponse) {
+  console.log(JSONResponse);
+  //var printThis = JSON.parse(JSONResponse);
+  //console.log(printThis);
+})
+.catch(function(error) {
+  console.log(error);
+});
 }
 
 function removeFromList(className){
     // alert("this works")
     return false;
-}
-
-.catch(err => {
-	console.error(err);
-});
-
- };
-
-saveIngre.addEventListener("click", createTaskHandler);
+};
